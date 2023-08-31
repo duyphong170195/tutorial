1402b0f6457752cfe6b5e3849dbbdd8b2ce5922a
phongnd13 <v.phongnd13@vinhomes.vn>
2023-08-31 17:23:40 +0700

    CombinationSum

--- hackerrank/src/leetcode/algorithms/CombinationSum.java
+++ hackerrank/src/leetcode/algorithms/CombinationSum.java
@@ -4,81 +4,124 @@ import java.util.*;
 
 public class CombinationSum {
 
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        Set<Integer> candidatesExisted = new HashSet<>();
-        for(int i = 0; i< candidates.length; i++) {
-            candidatesExisted.add(candidates[i]);
-        }
-
-        Map<Integer, List<List<Integer>>> keyToListNumbers = new HashMap<>();
-
-        for(int i = 1; i< candidates.length; i++) {
-            int key = candidates[i];
-            List<List<Integer>> combinationList = new ArrayList<>();
-            for(int j = 0 ; j < i; j++) {
-                int remainder = key % candidates[j];
-                if(remainder == 0 || candidatesExisted.contains(remainder)) {
-                    int resultDeviding = key / candidates[j];
-                    List<Integer> combinationOfEachNumber = new ArrayList<>();
-                    if(remainder != 0) {
-                        combinationOfEachNumber.add(remainder);
-                    }
-                    for(int k = 0; k < resultDeviding; k++) {
-                        combinationOfEachNumber.add(candidates[j]);
-                    }
-                    combinationList.add(combinationOfEachNumber);
-                }
-            }
-            keyToListNumbers.put(key, combinationList);
-        }
-        for(Map<Integer, List<List<Integer>>> )
-
-            // TODO check
-//        for(int m = 0; m < combinationOfEachNumber.size(); m++) {
-//            if(keyToListNumbers.containsKey(combinationOfEachNumber.get(i))) {
-//                List<List<Integer>> combinationSub =
-//                        List<Integer> combinationElements = new ArrayList<>();
-//                for(int n = 0; n < combinationOfEachNumber.size(); n++) {
-//                    if(n == i) {
-//                        combinationElements.addAll(keyToListNumbers.get(i));
-//                    } else {
-//                        combinationElements.add(combinationOfEachNumber.get(n));
+//    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+//
+//        Set<Integer> candidatesExisted = new HashSet<>();
+//        for(int i = 0; i< candidates.length; i++) {
+//            candidatesExisted.add(candidates[i]);
+//        }
+//
+//        TreeMap<Integer, List<List<Integer>>> keyToListNumbers = new TreeMap<>();
+//
+//        for(int i = 1; i< candidates.length; i++) {
+//            int key = candidates[i];
+//            List<List<Integer>> combinationList = new ArrayList<>();
+//            for(int j = i-1 ; j >= 0; j--) {
+//                int remainder = key % candidates[j];
+//                if(remainder == 0 || candidatesExisted.contains(remainder)) {
+//                    int resultDeviding = key / candidates[j];
+//                    List<Integer> combinationOfEachNumber = new ArrayList<>();
+//                    if(remainder != 0) {
+//                        combinationOfEachNumber.add(remainder);
+//                    }
+//                    for(int k = 0; k < resultDeviding; k++) {
+//                        combinationOfEachNumber.add(candidates[j]);
+//                    }
+//                    combinationList.add(combinationOfEachNumber);
+//
+//                    if(keyToListNumbers.containsKey(candidates[j])) {
+//
+//
+//
+//
+//                        List<List<Integer>> combinationsOfSumOfKey =  keyToListNumbers.get(candidates[j]);
+//                        for(int m = 0; m < combinationOfEachNumber.size(); m++) {
+//                            if(keyToListNumbers.containsKey(combinationOfEachNumber.get(i))) {
+//                                    List<Integer> combinationElements = new ArrayList<>();
+//
+//                                for(int n = 0; n < combinationOfEachNumber.size(); n++) {
+//                                    if(n == i) {
+//                                        combinationElements.addAll(keyToListNumbers.get(i));
+//                                    } else {
+//                                        combinationElements.add(combinationOfEachNumber.get(n));
+//                                    }
+//                                }
+//                            }
+//                        }
 //                    }
 //                }
+//
+//
+//            }
+//            keyToListNumbers.put(key, combinationList);
+//
+//
+//        }
+//
+//        Set<Integer> candidatesSet = new HashSet<>();
+//        for(int i = 0; i< candidates.length; i++) {
+//            candidatesSet.add(candidates[i]);
+//        }
+//        List<List<Integer>> result = new ArrayList<>();
+//        for(int i = 0; i < candidates.length; i++) {
+//            List<Integer> sum = new ArrayList<>();
+//            int remainder = target % candidates[i];
+//            if(remainder == 0) {
+//                int repeat = target / candidates[i] ;
+//                sum.addAll(Collections.nCopies(repeat, candidates[i]));
+//            } else {
+//
 //            }
 //        }
+//
+//        return new ArrayList<>();
+//    }
 
-
-
-
-        Set<Integer> candidatesSet = new HashSet<>();
-        for(int i = 0; i< candidates.length; i++) {
-            candidatesSet.add(candidates[i]);
-        }
-        List<List<Integer>> result = new ArrayList<>();
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
         for(int i = 0; i < candidates.length; i++) {
-            List<Integer> sum = new ArrayList<>();
-            int remainder = target % candidates[i];
-            if(remainder == 0) {
-                int repeat = target / candidates[i] ;
-                sum.addAll(Collections.nCopies(repeat, candidates[i]));
-            } else {
-
-            }
+            recursion(new ArrayList<>(List.of(candidates[i])), candidates, i, target, results);
         }
 
-        return new ArrayList<>();
+        return results;
+    }
+
+    public static void recursion(List<Integer> combination, int[] candidates, int startIndex, int target, List<List<Integer>> results) {
+        int sum = combination.stream().mapToInt(Integer::intValue)
+                .sum();
+
+        if(sum > target) {
+            return;
+        }
+
+        if(sum == target) {
+            results.add(combination);
+            return;
+        }
+
+
+        for(int i = startIndex; i < candidates.length; i++) {
+            List<Integer> newArray = new ArrayList<>();
+            newArray.addAll(combination);
+            newArray.add(candidates[i]);
+
+            recursion(newArray, candidates, startIndex, target, results);
+        }
     }
 
     public static void main(String[] args) {
-        int a[] = {2,3,4,6,7};
-        combinationSum(a, 6);
-        List<Integer> abc = new ArrayList<>();
-        abc.addAll(List.of(2,3,4,6,7));
 
-        abc.toString();
 
-        System.out.println(abc);
+        int a[] = {2,3,4,6,7, 9};
+
+
+        List<List<Integer>> results = combinationSum2(a, 13);
+//        combinationSum(a, 6);
+//        List<Integer> abc = new ArrayList<>();
+//        abc.addAll(List.of(2,3,4,6,7));
+//
+//        abc.toString();
+
+        System.out.println(results);
     }
 }
--- hackerrank/src/leetcode/algorithms/SwapNodesInPairs.java
+++ hackerrank/src/leetcode/algorithms/SwapNodesInPairs.java
@@ -3,59 +3,59 @@ package leetcode.algorithms;
 
 public class SwapNodesInPairs {
 
-    public static ListNodeSwap swapPairs(ListNodeSwap head) {
-        if(head.next == null) return head;
-
-        int i =1;
-        ListNodeSwap headTemp = null;
-        ListNodeSwap copyHead = head;
-        ListNodeSwap previousNode = null;
-        ListNodeSwap firstTemp = null;
-        while (copyHead != null) {
-            if(copyHead.next == null) {
-                return headTemp;
-            }
-
-            if(i % 2 == 0) {
-                previousNode = firstTemp;
-                i++;
-                continue;
-            }
-
-            // temp node 1
-            firstTemp = copyHead;
-            // temp node 2
-
-            ListNodeSwap sencondTemp = copyHead.next;
-
-            firstTemp.next = sencondTemp.next;
-
-            sencondTemp.next = firstTemp;
-
-
-            if(i > 2) {
-                previousNode.next = sencondTemp;
-            }
-
-            if(i == 1) {
-                headTemp = sencondTemp;
-            }
-            i++;
-            copyHead = copyHead.next;
-
-        }
-        return headTemp;
-    }
-
-    public static void main(String[] args) {
-//        ListNodeSwap a4 = new ListNodeSwap(4);
-        ListNodeSwap a3 = new ListNodeSwap(3);
-        ListNodeSwap a2 = new ListNodeSwap(2, a3);
-        ListNodeSwap a1 = new ListNodeSwap(1, a2);
-
-        ListNodeSwap head = swapPairs(a1);
-        System.out.println(head);
-    }
+//    public static ListNodeSwap swapPairs(ListNodeSwap head) {
+//        if(head.next == null) return head;
+//
+//        int i =1;
+//        ListNodeSwap headTemp = null;
+//        ListNodeSwap copyHead = head;
+//        ListNodeSwap previousNode = null;
+//        ListNodeSwap firstTemp = null;
+//        while (copyHead != null) {
+//            if(copyHead.next == null) {
+//                return headTemp;
+//            }
+//
+//            if(i % 2 == 0) {
+//                previousNode = firstTemp;
+//                i++;
+//                continue;
+//            }
+//
+//            // temp node 1
+//            firstTemp = copyHead;
+//            // temp node 2
+//
+//            ListNodeSwap sencondTemp = copyHead.next;
+//
+//            firstTemp.next = sencondTemp.next;
+//
+//            sencondTemp.next = firstTemp;
+//
+//
+//            if(i > 2) {
+//                previousNode.next = sencondTemp;
+//            }
+//
+//            if(i == 1) {
+//                headTemp = sencondTemp;
+//            }
+//            i++;
+//            copyHead = copyHead.next;
+//
+//        }
+//        return headTemp;
+//    }
+//
+//    public static void main(String[] args) {
+////        ListNodeSwap a4 = new ListNodeSwap(4);
+//        ListNodeSwap a3 = new ListNodeSwap(3);
+//        ListNodeSwap a2 = new ListNodeSwap(2, a3);
+//        ListNodeSwap a1 = new ListNodeSwap(1, a2);
+//
+//        ListNodeSwap head = swapPairs(a1);
+//        System.out.println(head);
+//    }
 
 
 
